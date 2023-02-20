87b827d7074a227882a731c8c3df789cb7b7f81a
fmahlaw <ferianmahlaw@gmail.com>
2023-02-20 21:38:34 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 
-function App() {
+const socket = io();
+
+function ChatApp() {
+  const [messages, setMessages] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    socket.on('chat-message', (message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+  }, []);
+
+  function handleInputValueChange(event) {
+    setInputValue(event.target.value);
+  }
+
+  function handleSendMessage() {
+    socket.emit('chat-message', inputValue);
+    setInputValue('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <div id="chat-box">
+        {messages.map((message, index) => (
+          <div key={index}>{message}</div>
+        ))}
+      </div>
+      <div>
+        <input
+          type="text"
+          id="message-input"
+          value={inputValue}
+          onChange={handleInputValueChange}
+        />
+        <button id="send-button" onClick={handleSendMessage}>
+          Send
+        </button>
+      </div>
     </div>
+    </>
   );
 }
 
-export default App;
+export default ChatApp;
